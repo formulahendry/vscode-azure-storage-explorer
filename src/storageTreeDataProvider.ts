21d328c8c9359ac37851214655d1af48b41a4e41
formulahendry <formulahendry@gmail.com>
2017-09-16 08:24:49 +0800

    Add AzureAccount ext as dependency

--- src/storageTreeDataProvider.ts
+++ src/storageTreeDataProvider.ts
@@ -1,10 +1,12 @@
 import * as path from "path";
 import * as vscode from "vscode";
+import { AzureAccount, AzureLoginStatus, AzureSession } from "./azure-account.api";
 
 export class StorageTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-    private containerStrings = [];
+    private readonly accountApi: AzureAccount;
 
     constructor(context: vscode.ExtensionContext) {
+        this.accountApi = vscode.extensions.getExtension<AzureAccount>("ms-vscode.azure-account")!.exports;
     }
 
     public getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
