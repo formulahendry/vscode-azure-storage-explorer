fc60d297fd87bc6aedfd957a143901a40e6954a7
formulahendry <formulahendry@gmail.com>
2017-09-18 11:50:02 +0800

    getResourceGroupNameById

--- src/model/SubscriptionNode.ts
+++ src/model/SubscriptionNode.ts
@@ -19,7 +19,7 @@ export class SubscriptionNode implements INode {
         const client = new storageManagementClient(this.azureResourceFilter.session.credentials, this.azureResourceFilter.subscription.subscriptionId);
         const nodes = await client.storageAccounts.list().then((storageAccounts) => {
             return storageAccounts.map(async (storageAccount) => {
-                const storageAccountKeys = await client.storageAccounts.listKeys("azureiot", storageAccount.name).then((_storageAccountKeys) => {
+                const storageAccountKeys = await client.storageAccounts.listKeys(this.getResourceGroupNameById(storageAccount.id), storageAccount.name).then((_storageAccountKeys) => {
                     return _storageAccountKeys.keys;
                 });
                 return new StorageAccountNode(storageAccount, storageAccountKeys);
@@ -28,5 +28,10 @@ export class SubscriptionNode implements INode {
         return Promise.all(nodes).then((_nodes) => {
             return _nodes;
         });
+    }
+
+    private getResourceGroupNameById(id: string): string {
+        const result = /\/resourceGroups\/([^/]+)\//.exec(id);
+        return result ? result[1] : "";
     }
 }
